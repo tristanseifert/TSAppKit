b32a3117faee0d5e5865b8f68b633c622c17c58f
Tristan Seifert <tristan@squee.co>
2014-11-10 20:10:55 -0600

    Initial Commit

---
+++ TSAppKit/TSAppKit.h
@@ -0,0 +1,19 @@
+//
+//  TSAppKit.h
+//  TSAppKit
+//
+//  Created by Tristan Seifert on 11/10/14.
+//  Copyright (c) 2014 Tristan Seifert. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for TSAppKit.
+FOUNDATION_EXPORT double TSAppKitVersionNumber;
+
+//! Project version string for TSAppKit.
+FOUNDATION_EXPORT const unsigned char TSAppKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TSAppKit/PublicHeader.h>
+
+
